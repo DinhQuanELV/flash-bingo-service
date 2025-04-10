441c2fe592c14ccc86cef438a590a5d6ad04febd
Dinh Quan <quanvonguyendinh@gmail.com>
2025-04-10 15:03:45 +0700

    set up cors nodemon morgan

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+require('dotenv').config();
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+// apply library
+app.use(
+  cors({
+    origin: process.env.BASE_URL || 'http://localhost:3000', // port fe
+    credentials: true, // allow call api
+  })
+);
+app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.send('hello world!');
