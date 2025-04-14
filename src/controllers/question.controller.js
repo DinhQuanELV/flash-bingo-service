0c27847639a8d8314b5e6d20f4c2c78de404a822
Dinh Quan <quanvonguyendinh@gmail.com>
2025-04-14 12:28:30 +0700

    feat(question): implement create api

--- src/controllers/question.controller.js
+++ src/controllers/question.controller.js
@@ -1,13 +1,50 @@
-const mongoose = require('mongoose');
-const { ObjectId } = mongoose.Types;
-
+const Module = require('../models/Module');
 const Question = require('../models/Question');
+const {
+  validateString,
+  validateArray,
+  validateObjectId,
+} = require('../utils/validateType');
 
 const questionController = {
   // [POST] /question/create/:moduleId
-  create: async () => {
+  create: async (req, res, next) => {
     try {
-      //
+      // get value
+      const { moduleId } = req.params;
+      const { keyword, title, correctAnswer, wrongAnswers } = req.body;
+
+      // validate value
+      validateObjectId(moduleId, 'moduleId');
+      validateString(keyword, 'keyword');
+      validateString(title, 'title');
+      validateString(correctAnswer, 'correctAnswer');
+      validateArray(wrongAnswers, 'wrongAnswers');
+
+      // check exist module
+      const module = await Module.findById(moduleId);
+      if (!module) {
+        return res.status(404).json({ message: 'Module not found!' });
+      }
+
+      // check exist question
+      const existsQuestion = await Question.findOne({ moduleId, title });
+      if (existsQuestion) {
+        return res.status(409).json({ message: 'Question already exists!' });
+      }
+
+      const question = new Question({
+        keyword,
+        title,
+        correctAnswer,
+        wrongAnswers,
+      });
+      await question.save();
+
+      return res.status(201).json({
+        message: 'Create question successfully!',
+        question,
+      });
     } catch (error) {
       next(error);
     }
