6e3204b1dc851948280a62257612ab955c37edd0
Dinh Quan <quanvonguyendinh@gmail.com>
2025-04-15 15:53:13 +0700

    feat(bingo): show card api

--- src/controllers/bingo.controller.js
+++ src/controllers/bingo.controller.js
@@ -18,10 +18,22 @@ const bingoController = {
     }
   },
 
-  // [GET] /bingo/show/:bingoId
+  // [GET] /bingo/show/:moduleId/:index
   show: async (req, res, next) => {
     try {
-      //
+      const { moduleId, index } = req.params;
+      const indexNum = parseInt(index, 10);
+
+      if (isNaN(indexNum) || indexNum < 1 || indexNum > 30) {
+        return res.status(400).json({ message: 'Index must be a number between 1 and 30!' });
+      }
+
+      const bingo = await Bingo.findOne({ moduleId, index }).populate('moduleId', 'title');
+      if (!bingo) {
+        return res.status(404).json({ message: 'Bingo not found!' });
+      }
+
+      return res.status(200).json(bingo);
     } catch (error) {
       next(error);
     }
